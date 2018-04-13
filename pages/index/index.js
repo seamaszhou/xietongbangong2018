405d43cc4879c0790bbf3adaefa990a22803566d
zhouseamas <zhouseamas@192-168-1-100.tpgi.com.au>
2018-04-13 15:56:15 +1000

    修改了主界面，增加了两个图标和搜索框

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,16 +8,16 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    a:1,
-    b:2,
-    c:3,
-    W:'1248124', 
+    a: 1,
+    b: 2,
+    c: 3,
+    W: '1248124',
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function(){
     wx.navigateTo({
-      url: '../main/main'
+      url: '../search/searchbar',
     })
   },
   onLoad: function () {
@@ -26,7 +26,7 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -48,7 +48,7 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
